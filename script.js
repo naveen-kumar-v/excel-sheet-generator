c6f06ce78790098c9548ad18a6867e77f6a819b6
NavinCGR <115581815+NavinCGR@users.noreply.github.com>
2023-05-01 23:02:39 +0530

    Added sweetalert.js to alert the user.

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
-let table = document.getElementsByClassName("sheet-body")[0],
-rows = document.getElementsByClassName("rows")[0],
-columns = document.getElementsByClassName("columns")[0]
-tableExists = false
+let table = document.getElementsByClassName("sheet-body")[0];
+let rows = document.getElementsByClassName("rows")[0];
+let columns = document.getElementsByClassName("columns")[0];
+let tableExists = false
 
 const generateTable = () => {
-    let rowsNumber = parseInt(rows.value), columnsNumber = parseInt(columns.value)
+    let rowsNumber = parseInt(rows.value);
+    let columnsNumber = parseInt(columns.value)
     table.innerHTML = ""
-    for(let i=0; i<rowsNumber; i++){
+
+    for (let i = 0; i < rowsNumber; i++) {
         var tableRow = ""
-        for(let j=0; j<columnsNumber; j++){
+        for (let j = 0; j < columnsNumber; j++) {
             tableRow += `<td contenteditable></td>`
         }
         table.innerHTML += tableRow
     }
-    if(rowsNumber>0 && columnsNumber>0){
+    if (rowsNumber > 0 && columnsNumber > 0) {
         tableExists = true
+    } else {
+        swal({
+            title: " Oops!",
+            text: " Please enter a valid number of row(s) and column(s)",
+            icon: "warning",
+            dangerMode: true,
+        });
     }
+
 }
 
 const ExportToExcel = (type, fn, dl) => {
-    if(!tableExists){
+    if (!tableExists) {
+        swal("Uh oh..", "Nothing to export :(");
         return
     }
     var elt = table
     var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" })
-    return dl ? XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' })
-        : XLSX.writeFile(wb, fn || ('MyNewSheet.' + (type || 'xlsx')))
+    return dl ? XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }) :
+        XLSX.writeFile(wb, fn || ('MyNewSheet.' + (type || 'xlsx')))
 }
